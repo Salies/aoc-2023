9dfbf2e95f2f33e44d0d9cd1b9db2df42eb86861
Salies <serezane@protonmail.com>
2023-12-23 04:19:29 -0300

    cleaner code

--- aoc/day03/first.py
+++ aoc/day03/first.py
@@ -12,34 +12,30 @@ def get_neighbors(m, x, y):
                     neighbors.append(m[i][j])
     return neighbors
 
-i = 0
-
 nums_to_add = []
 
-while i < len(lines):
-    j = 0
+for i in range(len(lines)):
     buf = ''
     will_add = False
-    while j < len(lines[i]):
+    for j in range(len(lines[i])):
         if lines[i][j].isdigit():
-            if not will_add:
-                # get neighbors
-                neighbors = get_neighbors(lines, i, j)
-                # if neighbor is not 0-9 or '.', will_add = True
-                for neighbor in neighbors:
-                    if neighbor != '.' and neighbor.isdigit() == False:
-                        will_add = True
-                        break
             buf += lines[i][j]
-        else:
             if will_add:
-                nums_to_add.append(int(buf))
-            buf = ''
-            will_add = False
-        j += 1
+                continue
+            # get neighbors
+            neighbors = get_neighbors(lines, i, j)
+            # if neighbor is not 0-9 or '.', will_add = True
+            for neighbor in neighbors:
+                if neighbor != '.' and neighbor.isdigit() == False:
+                    will_add = True
+                    break
+            continue
+        if will_add:
+            nums_to_add.append(int(buf))
+        buf = ''
+        will_add = False
     # do not ignore last in line
     if will_add and buf != '':
         nums_to_add.append(int(buf))
-    i += 1
 
 print(sum(nums_to_add))
